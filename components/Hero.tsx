a4607615d220675a3f9e767aea1aecf856385ed3
Beto Mossmann <gilbertomossmann@gmail.com>
2023-06-15 14:13:17 -0300

    footer a navbar components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { CustomButton } from '@/components'
 
 const Hero = () => {
-  const handleScrioll = () => {
+  const handleScroll = () => {
 
   }
 
@@ -22,11 +22,15 @@ const Hero = () => {
         <CustomButton
           title='Explore Cars'
           containerStyles='bg-primary-blue text-white rounded-full mt-10'
-          handleClick={handleScrioll}
+          handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src='/hero.png' alt='hero' fill className='object-contain' />
 
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   )
