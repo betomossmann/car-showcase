7b3d4a3f9fae27d835f7d59519972c6e63a211f4
Beto Mossmann <gilbertomossmann@gmail.com>
2023-06-26 09:10:09 -0300

    rename components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
             className='object-contain'
           />
           <p className='text-base text-gray-700'>
-            CarRental <br />
+            CarHub <br />
             All rights reserved &copy;
           </p>
         </div>
@@ -35,7 +35,7 @@ const Footer = () => {
       </div>
 
         <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
-          <p>@2023 CarRental. All Rights Reserved</p>
+          <p>@2023 CarHub. All Rights Reserved</p>
           <div className='footer__copyrights-link'>
             <Link href='/' className='text-gray-500'>
               Privacy Policy
